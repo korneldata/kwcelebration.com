5b302ffdf61a2e39ceeb4c7e047e6684f0afaa8b
korneldata <126023589+korneldata@users.noreply.github.com>
2023-09-26 21:44:08 +0200

    Update kwcelebration_web_scraper.py

--- kwcelebration_web_scraper.py
+++ kwcelebration_web_scraper.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[17]:
-
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -15,19 +9,23 @@ service = Service()
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
+#waiting until page is loaded
 driver.get('https://www.kwcelebration.com/our-agents/')
 wait = WebDriverWait(driver, 10)
 wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
 
+#finding the list of all agents
 agents_details = []
 agents_list = driver.find_elements(By.XPATH, '//div[@class="agent-information"]')
 
+#iterating over the list and finding all neccessary information
 for agent in agents_list:
     f_name = agent.find_element(By.CLASS_NAME, 'first').text
     l_name = agent.find_element(By.CLASS_NAME, 'last').text
     mobile = agent.find_element(By.TAG_NAME, 'a').get_attribute('href').replace('tel:', '')
     email = agent.find_element(By.XPATH, './/a[contains(@href, "mailto:")]').get_attribute('href')
-      
+
+#appending data to the list      
     agents_details.append({
         'first_name': f_name,
         'last_name': l_name,
@@ -37,6 +35,9 @@ for agent in agents_list:
 
 driver.quit()
 
+#converting list as a data frame
 df = pd.DataFrame(agents_details)
+
+#saving data to an Excel file
 df.to_excel(r'C:\Users\Nowy_użytkownik\Desktop\kwcelebration.xlsx',index=False)
 
